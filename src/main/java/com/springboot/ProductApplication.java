41c3185b503de79d98b894e24e09dcc1b62ee282
aakbatra <>
2020-05-02 01:41:55 +0530

    Hystrix implementation for product service

--- src/main/java/com/springboot/ProductApplication.java
+++ src/main/java/com/springboot/ProductApplication.java
@@ -2,12 +2,15 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 //@EnableEurekaClient
-//@EnableHystrix
+@EnableHystrix
+@EnableHystrixDashboard
 public class ProductApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springboot/controller/ProductRestController.java
+++ src/main/java/com/springboot/controller/ProductRestController.java
@@ -1,9 +1,6 @@
 package com.springboot.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springboot.exception.ProductNotFoundException;
 import com.springboot.model.Product;
-import com.springboot.repos.ProductRepo;
 import com.springboot.service.ProductRestService;
 
 @RestController
@@ -29,15 +26,48 @@ public class ProductRestController {
 	ProductRestService productService;
 
 	@RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+	@HystrixCommand(groupKey="ProductMicroService", fallbackMethod="getProductByIdFallback", commandKey="getProductById")
 	public Product getProductById(@PathVariable("id") String id) throws ProductNotFoundException {
-		System.out.println("Started!!!");
+		System.out.println("getProductById: Started!!!");
 		return productService.getProductById(id);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/products")
-	public void addProduct(@RequestBody Product product) throws JsonProcessingException {
+	@HystrixCommand(groupKey="ProductMicroService", fallbackMethod="addProductFallback")
+	public String addProduct(@RequestBody Product product) throws JsonProcessingException {
+		System.out.println("addProduct: Started!!!");
 		ObjectMapper mapper = new ObjectMapper();
 		productService.setProduct(String.valueOf(product.getId()), product);
+		return "SUCCESS";
+
+	}
+	
+	/**
+	 * Gets the product by id fallback.
+	 *
+	 * @param id
+	 *            the id
+	 * @return the product by id fallback
+	 */
+	public Product getProductByIdFallback(@PathVariable("id") String id, Throwable throwable) {
+		if(throwable instanceof ProductNotFoundException){
+			System.out.println("Product: " + id + " not found");
+		}else if(throwable instanceof Exception){
+			System.out.println("System is down!");
+		}
+		return new Product();
+	}
+
+	/**
+	 * Adds the product fallback.
+	 *
+	 * @param product
+	 *            the product
+	 * @return the string
+	 */
+	public String addProductFallback(@RequestBody Product product) {
+		System.out.println("Adding product: " + String.valueOf(product.getId()) + " failed");
+		return "FAILED";
 
 	}
 }
