6f8fe1882a93c6b4fe7369927202597642f660e5
abhtripa <abhishek.tripathi@publicissapient.com>
2020-05-02 11:54:16 +0530

    added discovery client

--- src/main/java/com/microservice/productservice/ProductApplication.java
+++ src/main/java/com/microservice/productservice/ProductApplication.java
@@ -2,13 +2,12 @@ package com.microservice.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrix
 @EnableHystrixDashboard
 public class ProductApplication {
