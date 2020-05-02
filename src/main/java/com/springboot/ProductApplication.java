a7e5e5031e8996659eb2ddb9ae5e2eb794320669
aakbatra <>
2020-05-02 18:30:16 +0530

    Changes done for supporting turbine implementation

--- src/main/java/com/springboot/ProductApplication.java
+++ src/main/java/com/springboot/ProductApplication.java
@@ -2,15 +2,12 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableHystrix
-@EnableHystrixDashboard
 public class ProductApplication {
 
 	public static void main(String[] args) {
